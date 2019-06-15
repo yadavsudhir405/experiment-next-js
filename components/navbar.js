27a9af9fee4a7fa05366d53b8dc2a12c07efe336
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-15 20:57:53 +0530

    Added the style inside the component

--- components/navbar.js
+++ components/navbar.js
@@ -2,12 +2,27 @@ import Link from 'next/Link';
 
 const Navbar = () => (
     <div>
-        <Link href='/'>
-            <a>Home</a>
-        </Link> |
-        <Link href='/about'>
-            <a> About</a>
-        </Link>
+        <ul>
+            <li>
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+            </li>
+            <li>
+                <Link href='/about'>
+                    <a> About</a>
+                </Link>
+            </li>
+        </ul>
+        <style jsx>{`
+             ul {
+                background: hsl(24, 100%, 50%);
+                color: hsl(0, 0%, 0%);
+                list-style: none;
+                display: flex;
+             }
+        `}
+        </style>
     </div>
 
 );
