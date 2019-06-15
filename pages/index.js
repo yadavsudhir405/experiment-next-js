ee3f05a34055d9d23dfd29385c3171dc63472a36
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-15 14:15:25 +0530

    First commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+class Index extends React.Component{
+    render() {
+        return (
+            <div>
+                Welcome to Next js
+            </div>
+        );
+    }
+}
+
+export default Index;
