5079e77f7d18f909a16bd92176e2923bfa0156ce
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-15 14:30:03 +0530

    Page with next js style

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-export default class Index extends React.Component{
-    render() {
+const Index = ()=> {
         return (
             <div>
                  Edited  Welcome to Next js
             </div>
         );
-    }
-}
+};
+
+export default Index;
 
