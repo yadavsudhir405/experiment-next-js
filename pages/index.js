7e6326ad5ca5b7ecb92159e76000e631f67fa402
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-15 14:26:13 +0530

    Create Page with class style

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-class Index extends React.Component{
+export default class Index extends React.Component{
     render() {
         return (
             <div>
-                Welcome to Next js
+                 Edited  Welcome to Next js
             </div>
         );
     }
 }
 
-export default Index;
