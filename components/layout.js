021c18546bbb4991e8af5de9f19f7a009ad78ed7
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-06-18 17:58:47 +0530

    Added the clock component to layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,15 @@
 import Navbar from '../components/navbar';
+import Head from 'next/head';
+import Clock from '../components/Clock';
 
 const Layout = (props) => (
     <div>
+        <Head>
+            <title>Next Demo</title>
+        </Head>
         <div>
             <Navbar/>
+            <Clock/>
             {props.children}
         </div>
     </div>
