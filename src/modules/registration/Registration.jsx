72aad2516b96a6285d8afba98c547bbe91785918
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-07-07 21:33:53 +0530

    Add custom styling to button using styled Higher order component

--- src/modules/registration/Registration.jsx
+++ src/modules/registration/Registration.jsx
@@ -1,25 +1,26 @@
-import Button from '@material-ui/core/Button';
-import { styled } from '@material-ui/styles';
-
 import RegistrationForm from './RegistratonForm';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/styles';
 
-const MyButton = styled(Button)({
-  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-});
+const styles = {
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  }
+};
 
-const Registration = () => {
+const Registration = (props) => {
   return (
     <>
-      <MyButton>Hello</MyButton>
+      <Button className={props.classes.root}>Hello</Button>
       <RegistrationForm />
     </>
   );
 };
 
-export default Registration;
+export default withStyles(styles)(Registration);
